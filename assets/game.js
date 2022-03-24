80a83fab13a930aaa0c0749cf13d4eed838855aa
Hephaestus <mcmahonmike9@yahoo.com>
2022-03-23 21:26:50 -0500

    created base variables and functions

---
+++ assets/game.js
@@ -0,0 +1,19 @@
+var startgameEl = document.getElementById('start-button')
+var questionsEl = document.getElementById('quiz-questions')
+var answersEl = document.getElementById('answer-btns')
+
+
+function createQuestions (){
+
+}
+
+function createAnswers () {
+    
+}
+
+
+function startQuiz () {
+
+}
+
+startQuiz()
