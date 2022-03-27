a05d4558443fd62493454fdef145b49e9c4a53c3
Hephaestus <mcmahonmike9@yahoo.com>
2022-03-27 18:44:27 -0500

    comments added to game.js

--- assets/game.js
+++ assets/game.js
@@ -13,6 +13,7 @@ var score = 0
 var scoreEl = document.getElementById('score')
 var questionIndex = 0
 var intervalID
+//Quiz Questions Array
 var questionsArray = [
     {
         question: "What is the proper syntax for an array?",
@@ -23,41 +24,41 @@ var questionsArray = [
         correctAnswer: "[ ] square brackets"
     },
     {
-        question: "What?",
-        answer1: "t",
-        answer2: "m",
-        answer3: "m",
-        answer4: "f",
-        correctAnswer: "t"
+        question: "Which is not an example of primitive data?",
+        answer1: "String",
+        answer2: "Boolean",
+        answer3: "Number",
+        answer4: "Function",
+        correctAnswer: "Function"
     },
     {
-        question: "Why?",
-        answer1: "s",
-        answer2: "d",
-        answer3: "m",
-        answer4: "f",
-        correctAnswer: "d"
+        question: "What does parseInt() execute?",
+        answer1: "Converts strings to integers",
+        answer2: "Converts numbers to strings",
+        answer3: "Converts integers to strings",
+        answer4: "None of the above",
+        correctAnswer: "Converts strings to integers"
     },
     {
-        question: "Why?",
-        answer1: "s",
-        answer2: "d",
-        answer3: "m",
-        answer4: "f",
-        correctAnswer: "d"
+        question: "How can you generate a new element with JavaScript?",
+        answer1: ".getElementById",
+        answer2: ".querySelector",
+        answer3: ".setAttribute",
+        answer4: ".createElement",
+        correctAnswer: ".createElement"
     },
     {
-        question: "Why?",
-        answer1: "s",
-        answer2: "d",
-        answer3: "m",
-        answer4: "f",
-        correctAnswer: "d"
+        question: "Where must global variables be declared?",
+        answer1: "Inside the function",
+        answer2: "Outside the function",
+        answer3: "Both are true",
+        answer4: "Neither is true",
+        correctAnswer: "Outside the function"
     },   
     
 ]
 
-
+//Function to generate questions and create answer buttons 
 
 var createQuestions = function (){
  questionsEl.innerText = questionsArray[questionIndex].question
@@ -94,27 +95,33 @@ var createQuestions = function (){
  
 }
 
+//Function for event listeners on the answer buttons
 var answerAction = function (event) {
     event.stopPropagation()
     event.preventDefault()
     var answerClicked = event.target.innerText
-   
+   //generates the next question in the question array once a button has been clicked
     questionIndex++
-    
+    //if statement for if the selected answer is correct
     if (answerClicked === questionsArray[questionIndex-1].correctAnswer){
-        window.alert('correct')
+        window.alert('Correct!')
+        //adds a point the score counter if answer is correct
         score++
         scoreEl.innerText = score
-        
+        //else function if answer is incorrect
     } else {
-        window.alert('wrong')
+        window.alert('Incorrect!')
+        //subtraction of 10 seconds from the quiz timer
         time -= 10
        
     }
+    
     startQuiz()
     
         
     }
+
+    //function to start the count down timer
 function startTimer() {
     intervalID = setInterval(function() {
         time--
@@ -126,11 +133,13 @@ function startTimer() {
     }
    
 
-    
+//function to run once the quiz has been completed    
 var finishQuiz = function () {
     clearInterval(intervalID)
     var initials = prompt('What are you initials?')
+    //object for the initials and score
     var data = { initials: initials, score: score }
+    //storing the object and converting it to a string in local storage
     localStorage.setItem('highScore', JSON.stringify(data))
     var playAgain = confirm('Want to play again?')
     if (playAgain) {
@@ -142,7 +151,7 @@ var finishQuiz = function () {
 
 
 
-
+//start quiz function that will clear the intial page and run the createQuestions and startTimer function
 function startQuiz () {
 
 
@@ -157,5 +166,5 @@ startTimer();
 }
 
 
-
+//event listener added to start button tho initiate the startQuiz function and start the quiz
 buttonEl.addEventListener('click', startQuiz)
